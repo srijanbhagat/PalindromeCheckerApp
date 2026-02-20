707a9e5c9fe8532bcf15e1b1ae0823b3991f0ccc
srijanbhagat <srijanbhagat8@gmail.com>
2026-02-20 09:28:59 +0530

    [srijanbhagat] Added UC1-WelcomePage

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+/*
+Use case 1: Application Entry and Welcome Message
+
+Description :
+
+this is a palindrome checker app
+
+in this app we are going to check whether the given input by the user is a palindrome or not
+
+First lets know what is a palindrome , a palindrome is a word which when read both normally and from backwords will be the same
+for example : DAD if we read it normally it will be DAD and if i read it backwards it will be the same only
+
+This is the use case 1 part of the app
+we have successfully printed the welcome message and the version of the app
+
+our goal is to establish a clear startup flow
+
+@author - Srijan
+@version - 1.0
+
+*/
+public class PalindromeCheckerApp {
+    public  static void main(String[] args){
+        System.out.println("Welcome to Palindrone Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# PalindroneCheckerApp
+# PalindromeCheckerApp
+Use case 1: Application Entry and Welcome Message
+
+Description :
+
+This is a palindrome checker app
+
+In this app we are going to check whether the given input by the user is a palindrome or not
+
+First lets know what is a palindrome , a palindrome is a word which when read both normally and from backwords will be the same
+. For example : DAD if we read it normally it will be DAD and if i read it backwards it will be the same only
+
+This is the use case 1 part of the app
+We have successfully printed the welcome message and the version of the app
+
+Our goal is to establish a clear startup flow
+
+Language used - JAVA
+
+@author - Srijan
+@version - 1.0
